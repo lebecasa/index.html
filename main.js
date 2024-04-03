c7f0db03589a56daadc936d34b874beeea641299
lebecasa <148072673+lebecasa@users.noreply.github.com>
2024-04-03 09:16:13 -0300

    Update main.js

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2029-12-18T00:00:00");
-const tempoObjetivo2 = new Date("2024-03-25T00:00:00");
+const tempoObjetivo2 = new Date("2025-01-26T00:00:00");
 const tempoObjetivo3 = new Date("2024-03-25T00:00:00");
 const tempoObjetivo4 = new Date("2024-03-25T00:00:00");
 
